81adb935259a1986e03e56abec5ad2bc602b1a31
Eskeptor <skyvvv624@naver.com>
2023-01-11 21:52:37 +0900

    Update 2023.01.11

--- TetrisCpp/Player.h
+++ TetrisCpp/Player.h
@@ -1,8 +1,16 @@
+/**
+@brief			Player class
+@author			Eskeptor
+@date			2023-01-11
+@version		0.0.2
+*/
+
 #pragma once
 
 class CPlayer
 {
 public:
+	// Block direction
 	enum eDirection
 	{
 		Dir0 = 0,
@@ -14,54 +22,103 @@ public:
 	};
 
 private:
+	// Current X Position
 	int m_nXPos;
-	int m_nYPos;
-	int m_nCurBlock;
+	// Minimum X Position
 	int m_nMinXPos;
-	int m_nMinYPos;
+	// Maximum X Position
 	int m_nMaxXPos;
+	// Current Y Position
+	int m_nYPos;
+	// Minimum Y Position
+	int m_nMinYPos;
+	// Maximum Y Position
 	int m_nMaxYPos;
+	// Current Block Index
+	int m_nCurBlock;
+	// Current Block Direction
 	eDirection m_eDirection;
 
 public:
+	/**
+	@brief		Initialize
+	*/
 	CPlayer(int nXPos = 0, int nYPos = 0, int nCurBlock = 0, eDirection eDir = eDirection::Dir0)
 		: m_nXPos(nXPos), m_nYPos(nYPos), m_nCurBlock(nCurBlock), m_eDirection(eDir)
 		, m_nMinXPos(0), m_nMinYPos(0), m_nMaxXPos(20), m_nMaxYPos(20)
 	{}
 
+	/**
+	@brief		Set x position range
+	@param		nMinXPos	Minimum x position
+	@param		nMaxXPos	Maximum x position
+	@return
+	*/
 	inline void SetXPositionRange(int nMinXPos, int nMaxXPos)
 	{
 		m_nMinXPos = nMinXPos;
 		m_nMaxXPos = nMaxXPos;
 	}
 
+	/**
+	@brief		Set y position range
+	@param		nMinYPos	Minimum y position
+	@param		nMaxYPos	Maximum y position
+	@return
+	*/
 	inline void SetYPositionRange(int nMinYPos, int nMaxYPos)
 	{
 		m_nMinYPos = nMinYPos;
 		m_nMaxYPos = nMaxYPos;
 	}
 
+	/**
+	@brief		Set current position
+	@param		nXPos		Current x position
+	@param		nYPos		Current y position
+	@return
+	*/
 	inline void SetPosition(int nXPos, int nYPos)
 	{
 		m_nXPos = nXPos;
 		m_nYPos = nYPos;
 	}
 
+	/**
+	@brief		Set current x position
+	@param		nXPos		Current x position
+	@return
+	*/
 	inline void SetXPosition(int nXPos)
 	{
 		m_nXPos = nXPos;
 	}
 
+	/**
+	@brief		Set current y position
+	@param		nYPos		Current y position
+	@return
+	*/
 	inline void SetYPosition(int nYPos)
 	{
 		m_nYPos = nYPos;
 	}
 
+	/**
+	@brief		Set current block direction
+	@param		eDir		Current block direction
+	@return
+	*/
 	inline void SetDirection(eDirection eDir)
 	{
 		m_eDirection = eDir;
 	}
 
+	/**
+	@brief		Set current block direction to next direction
+	@param
+	@return
+	*/
 	inline void SetNextDirection()
 	{
 		if (m_eDirection + 1 >= eDirection::DirMax)
@@ -70,37 +127,83 @@ public:
 			m_eDirection = (eDirection)(m_eDirection + 1);
 	}
 
+	/**
+	@brief		Set current position (Increase by adder value)
+	@param		nXAdder		X Increased value
+	@param		nYAdder		Y Increased value
+	@return
+	*/
 	inline void AddPosition(int nXAdder, int nYAdder)
 	{
 		m_nXPos = (m_nXPos + nXAdder >= m_nMinXPos) ? (m_nXPos + nXAdder <= m_nMaxXPos ? m_nXPos + nXAdder : m_nMaxXPos) : m_nMinXPos;
 		m_nYPos = (m_nYPos + nYAdder >= m_nMinYPos) ? (m_nYPos + nYAdder <= m_nMaxYPos ? m_nYPos + nYAdder : m_nMaxYPos) : m_nMinYPos;
 	}
 
+	/**
+	@brief		Set current block index
+	@param		nBlock		Current block index
+	@return
+	*/
 	inline void SetBlock(int nBlock)
 	{
 		m_nCurBlock = nBlock;
 	}
 
+	/**
+	@brief		Get current x position
+	@param		
+	@return		Current x position
+	*/
 	inline int GetXPosition() const
 	{
 		return m_nXPos;
 	}
 
+	/**
+	@brief		Get current y position
+	@param
+	@return		Current y position
+	*/
 	inline int GetYPosition() const
 	{
 		return m_nYPos;
 	}
-
+	
+	/**
+	@brief		Get current block index
+	@param
+	@return		Current block index
+	*/
 	inline int GetBlock() const
 	{
 		return m_nCurBlock;
 	}
 
+	/**
+	@brief		Get current block direction
+	@param
+	@return		Current block direction
+	*/
 	inline eDirection GetDirection() const
 	{
 		return m_eDirection;
 	}
 
+	/**
+	@brief		Get next block direction
+	@param
+	@return		Next block direction
+	*/
+	inline eDirection GetNextDirection() const
+	{
+		return (m_eDirection + 1 >= eDirection::DirMax) ? eDirection::Dir0 : (eDirection)(m_eDirection + 1);
+	}
+
+	/**
+	@brief		Get current player position (COORD)
+	@param
+	@return		Current player position (COORD)
+	*/
 	inline COORD GetCursor()
 	{
 		COORD cursor{ (SHORT)m_nXPos, (SHORT)m_nYPos };
--- TetrisCpp/main.cpp
+++ TetrisCpp/main.cpp
@@ -1,12 +1,28 @@
+/**
+@brief			Main
+@author			Eskeptor
+@date			2023-01-11
+@version		0.0.2
+*/
+
 #include <iostream>
+#include <random>
 #include <Windows.h>
 #include <conio.h>
 #include "Player.h"
 
-// TODO Hit 판정 만들기
-
 using namespace std;
 
+// Map Width (constexpr)
+constexpr int MAP_WIDTH = 12;
+// Map Height (constexpr)
+constexpr int MAP_HEIGHT = 22;
+// Block Width (constexpr)
+constexpr int BLOCK_WIDTH = 4;
+// Block Height (constexpr)
+constexpr int BLOCK_HEIGHT = 4;
+
+// Key Code
 enum eKeyCode
 {
 	KEY_UP = 72,
@@ -16,12 +32,14 @@ enum eKeyCode
 	KEY_SPACE = 32,
 };
 
+// Rectangle Structure
 struct stRect
 {
 	int nWidth;
 	int nHeight;
 };
 
+// Console Structure
 struct stConsole
 {
 	// Console Handler
@@ -32,13 +50,27 @@ struct stConsole
 	HANDLE hBuffer[2];
 	// Current Console Buffer Index
 	int nCurBuffer;
+
+	// Random Seed
+	random_device rdDevice;
+	// Random Generation
+	mt19937 rdGen;
+	// Random Distribution (Block)
+	uniform_int_distribution<int> rdBlockDist;
+	// Random Distribution (Direction)
+	uniform_int_distribution<int> rdDirDist;
+
+	// Clock
+	clock_t timeStart;
+
+	stConsole()
+		: hConsole(nullptr), hBuffer{ nullptr, }, nCurBuffer(0)
+		, rdGen(rdDevice()), rdBlockDist(0, 6), rdDirDist(CPlayer::eDirection::Dir0, CPlayer::eDirection::Dir270)
+		, timeStart(clock())
+	{}
 };
 
-constexpr int MAP_WIDTH = 12;
-constexpr int MAP_HEIGHT = 22;
-constexpr int BLOCK_WIDTH = 4;
-constexpr int BLOCK_HEIGHT = 4;
-
+// Map Data
 int snArrMap[MAP_HEIGHT][MAP_WIDTH] = 
 {
 	{1,1,1,1,1,1,1,1,1,1,1,1,},
@@ -65,30 +97,40 @@ int snArrMap[MAP_HEIGHT][MAP_WIDTH] =
 	{1,1,1,1,1,1,1,1,1,1,1,1,},
 };
 
+// Map Data (Backup Data)
+int snArrMapBackup[MAP_HEIGHT][MAP_WIDTH] = { 0, };
+
+// Block Data
 const int BLOCKS[][BLOCK_WIDTH * BLOCK_HEIGHT] =
 {
-	{ 0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,1,0,0,0,1,1,0 },
-	{ 0,0,0,0,0,0,1,0,0,0,1,0,0,1,1,0 },
-	{ 0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,1,1,1,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,1,0,0,1,1,0,0,1,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,1,1,0,0,0,1,0 },
+	{ 0,0,0,0,2,2,2,2,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,2,0,0,0,2,0,0,0,2,2,0 },
+	{ 0,0,0,0,0,0,2,0,0,0,2,0,0,2,2,0 },
+	{ 0,0,0,0,0,2,2,0,0,2,2,0,0,0,0,0 },
+	{ 0,0,0,0,0,2,0,0,2,2,2,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,2,0,0,2,2,0,0,2,0,0 },
+	{ 0,0,0,0,0,2,0,0,0,2,2,0,0,0,2,0 },
 };
 
-const char BLOCK_TYPES[][3] =
+// Block Type
+const char BLOCK_TYPES[][4] =
 {
 	"  ",
-	"■",
+	"▣",
+	"□"
 };
 
-CPlayer player;
-int* pCurBlock = nullptr;
-stConsole console{ 0, };
-
+// Player Data
+CPlayer g_player;
+// Previous Player Data
+CPlayer g_prevPlayerData;
+// Block Pointer
+int* g_pCurBlock = nullptr;
+// Console Data
+stConsole console;
 
 /**
-@brief		Clear Console Screen
+@brief		Function that screen clear
 @param
 @return
 */
@@ -103,22 +145,33 @@ void ClearScreen()
 }
 
 /**
-@brief		Get Rotate Block Data
+@brief		Function that Flip screen buffer
+@param
+@return
+*/
+void BufferFlip()
+{
+	SetConsoleActiveScreenBuffer(console.hBuffer[console.nCurBuffer]);
+	console.nCurBuffer = console.nCurBuffer ? 0 : 1;
+}
+
+/**
+@brief		Funtion that get rotate block data
 @param		nBlockIdx		Block Index
 @param		eDir			Rotate Direction
 @return		Block
 */
 int* GetRotateBlock(int nBlockIdx, CPlayer::eDirection eDir)
 {
-	if (pCurBlock != nullptr)
+	if (g_pCurBlock != nullptr)
 	{
-		delete[] pCurBlock;
-		pCurBlock = nullptr;
+		delete[] g_pCurBlock;
+		g_pCurBlock = nullptr;
 	}
 
-	pCurBlock = new int[BLOCK_HEIGHT * BLOCK_WIDTH];
+	g_pCurBlock = new int[BLOCK_HEIGHT * BLOCK_WIDTH];
 	int nMemSize = sizeof(int) * BLOCK_HEIGHT * BLOCK_WIDTH;
-	memcpy_s(pCurBlock, nMemSize, BLOCKS[nBlockIdx], nMemSize);
+	memcpy_s(g_pCurBlock, nMemSize, BLOCKS[nBlockIdx], nMemSize);
 
 	for (int nRot = 0; nRot < (int)eDir; ++nRot)
 	{
@@ -128,18 +181,38 @@ int* GetRotateBlock(int nBlockIdx, CPlayer::eDirection eDir)
 		{
 			for (int nX = 0; nX < BLOCK_WIDTH; ++nX)
 			{
-				nTemps[(nX * BLOCK_WIDTH) + (BLOCK_HEIGHT - nY - 1)] = pCurBlock[(nY * BLOCK_HEIGHT) + nX];
+				nTemps[(nX * BLOCK_WIDTH) + (BLOCK_HEIGHT - nY - 1)] = g_pCurBlock[(nY * BLOCK_HEIGHT) + nX];
 			}
 		}
 
-		memcpy_s(pCurBlock, nMemSize, nTemps, nMemSize);
+		memcpy_s(g_pCurBlock, nMemSize, nTemps, nMemSize);
 	}
 
-	return pCurBlock;
+	return g_pCurBlock;
 }
 
 /**
-@brief		Initialize Game Data & Settings Function
+@brief		Function that get random block
+@param
+@return		Random block index
+*/
+int RandomBlock()
+{
+	return console.rdBlockDist(console.rdGen);
+}
+
+/**
+@brief		Function that get random direction
+@param
+@return		Random direction index
+*/
+int RamdomDirection()
+{
+	return console.rdDirDist(console.rdGen);
+}
+
+/**
+@brief		Function that initialize game data & Settings
 @param
 @return
 */
@@ -147,10 +220,13 @@ void InitGame()
 {
 	// Initialize Player Data
 	{
-		player.SetPosition(4, 1);
-		player.SetXPositionRange(0, MAP_WIDTH);
-		player.SetYPositionRange(0, MAP_HEIGHT);
-		player.SetBlock(2);
+		g_player.SetPosition(4, 1);
+		g_player.SetXPositionRange(0, MAP_WIDTH);
+		g_player.SetYPositionRange(0, MAP_HEIGHT);
+		g_player.SetBlock(RandomBlock());
+		g_player.SetDirection((CPlayer::eDirection)RamdomDirection());
+
+		g_prevPlayerData = g_player;
 	}
 
 	// Initialize Console Data
@@ -166,34 +242,33 @@ void InitGame()
 		console.rtConsole.nHeight = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top;
 
 		console.hBuffer[0] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-		//SetConsoleScreenBufferSize(console.hBuffer[0], consoleInfo.dwSize);
-		//SetConsoleWindowInfo(console.hBuffer[0], TRUE, &consoleInfo.srWindow);
+		SetConsoleScreenBufferSize(console.hBuffer[0], consoleInfo.dwSize);
+		SetConsoleWindowInfo(console.hBuffer[0], TRUE, &consoleInfo.srWindow);
 		SetConsoleCursorInfo(console.hBuffer[0], &consoleCursor);
 
 		console.hBuffer[1] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-		//SetConsoleScreenBufferSize(console.hBuffer[1], consoleInfo.dwSize);
-		//SetConsoleWindowInfo(console.hBuffer[1], TRUE, &consoleInfo.srWindow);
+		SetConsoleScreenBufferSize(console.hBuffer[1], consoleInfo.dwSize);
+		SetConsoleWindowInfo(console.hBuffer[1], TRUE, &consoleInfo.srWindow);
 		SetConsoleCursorInfo(console.hBuffer[1], &consoleCursor);
+	}
+
+	// Map Backup
+	{
+		memcpy_s(snArrMapBackup, sizeof(int) * MAP_WIDTH * MAP_HEIGHT, snArrMap, sizeof(int) * MAP_WIDTH * MAP_HEIGHT);
 	}
 }
 
-void BufferFlip()
-{
-	SetConsoleActiveScreenBuffer(console.hBuffer[console.nCurBuffer]);
-	console.nCurBuffer = console.nCurBuffer ? 0 : 1;
-}
-
 /**
-@brief		Destroy Game Data
+@brief		Function to erase game data.
 @param
 @return
 */
 void DestroyGame()
 {
-	if (pCurBlock != nullptr)
+	if (g_pCurBlock != nullptr)
 	{
-		delete[] pCurBlock;
-		pCurBlock = nullptr;
+		delete[] g_pCurBlock;
+		g_pCurBlock = nullptr;
 	}
 
 	if (console.hBuffer[0] != nullptr)
@@ -208,11 +283,11 @@ void DestroyGame()
 }
 
 /**
-@brief		Draw Frame Function
+@brief		Rendering function
 @param
 @return
 */
-void DrawFrame()
+void Render()
 {
 	COORD coord{ 0, };
 	int nXAdd = 0;
@@ -228,38 +303,34 @@ void DrawFrame()
 			coord.Y = nY;
 
 			SetConsoleCursorPosition(console.hBuffer[console.nCurBuffer], coord);
-			WriteFile(console.hBuffer[console.nCurBuffer], BLOCK_TYPES[snArrMap[nY][nX]], sizeof(char) * 3, &dw, NULL);
+			WriteFile(console.hBuffer[console.nCurBuffer], BLOCK_TYPES[snArrMap[nY][nX]], sizeof(char) * 4, &dw, NULL);
 
 			if (snArrMap[nY][nX] == 0)
 			{
-				coord.X = ++nXAdd;
 				SetConsoleCursorPosition(console.hBuffer[console.nCurBuffer], coord);
-				WriteFile(console.hBuffer[console.nCurBuffer], BLOCK_TYPES[snArrMap[nY][nX]], sizeof(char) * 3, &dw, NULL);
+				WriteFile(console.hBuffer[console.nCurBuffer], BLOCK_TYPES[snArrMap[nY][nX]], sizeof(char) * 4, &dw, NULL);
 			}
 
-			++nXAdd;
-			//printf("%s", BLOCK_TYPES[snArrMap[nY][nX]]);
+			// Special symbol size is 2 space.
+			nXAdd += 2;
 		}
-		//printf("\n");
 	}
-	//printf("\n");
 }
 
 /**
-@brief		Draw Player Function
+@brief		Function that puts blocks into a map to match the player's position.
 @param
 @return
 */
-void DrawPlayer()
+void CalcPlayer()
 {
-	static CPlayer sprevPlayerData = player;
-	COORD playerCursor = player.GetCursor();
+	COORD playerCursor = g_player.GetCursor();
 
 	// 이전 위치의 블록 제거
-	if (sprevPlayerData != player)
+	if (g_prevPlayerData != g_player)
 	{
-		int* pBlock = GetRotateBlock(sprevPlayerData.GetBlock(), sprevPlayerData.GetDirection());
-		COORD sprevCursor = sprevPlayerData.GetCursor();
+		int* pBlock = GetRotateBlock(g_prevPlayerData.GetBlock(), g_prevPlayerData.GetDirection());
+		COORD sprevCursor = g_prevPlayerData.GetCursor();
 
 		for (int nY = 0; nY < BLOCK_HEIGHT; ++nY)
 		{
@@ -271,10 +342,10 @@ void DrawPlayer()
 			}
 		}
 
-		sprevPlayerData = player;
+		g_prevPlayerData = g_player;
 	}
 
-	int* pBlock = GetRotateBlock(player.GetBlock(), player.GetDirection());
+	int* pBlock = GetRotateBlock(g_player.GetBlock(), g_player.GetDirection());
 	for (int nY = 0; nY < BLOCK_HEIGHT; ++nY)
 	{
 		for (int nX = 0; nX < BLOCK_WIDTH; ++nX)
@@ -286,7 +357,84 @@ void DrawPlayer()
 }
 
 /**
-@brief		Input Key Function
+@brief		Function to determine if there is a collision
+@param		pBlock		Block
+@param		coordPlayer	Player coord
+@return		true: Collision, false: Not collision
+*/
+bool IsCollision(int* pBlock, const COORD& coordPlayer)
+{
+	int nColision = 0;
+
+	for (int nY = 0; nY < BLOCK_HEIGHT; ++nY)
+	{
+		for (int nX = 0; nX < BLOCK_WIDTH; ++nX)
+		{
+			nColision = pBlock[(nY * BLOCK_HEIGHT) + nX] & (snArrMapBackup[coordPlayer.Y + nY][coordPlayer.X + nX] << 1);
+			nColision += pBlock[(nY * BLOCK_HEIGHT) + nX] & snArrMapBackup[coordPlayer.Y + nY][coordPlayer.X + nX];
+
+			if (nColision > 0)
+				return true;
+		}
+	}
+
+	return false;
+}
+
+/**
+@brief		Function to check if it can move horizontally
+@param		nXAdder		X position adder
+@param		nYAdder		Y position adder
+@return		true: Available, false: Unavailable
+*/
+bool IsMoveAvailable(int nXAdder, int nYAdder)
+{
+	COORD coorNext = g_player.GetCursor();
+	coorNext.X += nXAdder;
+	coorNext.Y += nYAdder;
+	int* pBlock = GetRotateBlock(g_player.GetBlock(), g_player.GetDirection());
+
+	return !IsCollision(pBlock, coorNext);
+}
+
+/**
+@brief		Function to check if it can rotate
+@param
+@return		true: Available, false: Unavailable
+*/
+bool IsRotateAvailable()
+{
+	int* pBlock = GetRotateBlock(g_player.GetBlock(), g_player.GetNextDirection());
+
+	return !IsCollision(pBlock, g_player.GetCursor());
+}
+
+/**
+@brief		Function to determine if the bottom has been reached
+@param
+@return
+*/
+void CheckBottom()
+{
+	if (IsMoveAvailable(0, 1) == false)
+	{
+		double dTimeDiff = clock() - console.timeStart;
+
+		if (dTimeDiff >= 1000)
+		{
+			memcpy_s(snArrMapBackup, sizeof(int) * MAP_WIDTH * MAP_HEIGHT, snArrMap, sizeof(int) * MAP_WIDTH * MAP_HEIGHT);
+
+			g_player.SetPosition(4, 1);
+			g_player.SetBlock(RandomBlock());
+			g_player.SetDirection((CPlayer::eDirection)RamdomDirection());
+
+			g_prevPlayerData = g_player;
+		}
+	}
+}
+
+/**
+@brief		Function that detects input keys.
 @param
 @return
 */
@@ -303,18 +451,51 @@ void InputKey()
 			case eKeyCode::KEY_UP:
 				break;
 			case eKeyCode::KEY_DOWN:
-				player.AddPosition(0, 1);
+				if (IsMoveAvailable(0, 1))
+				{
+					g_player.AddPosition(0, 1);
+					console.timeStart = clock();
+				}
 				break;
 			case eKeyCode::KEY_LEFT:
-				player.AddPosition(-1, 0);
+				if (IsMoveAvailable(-1, 0))
+				{
+					g_player.AddPosition(-1, 0);
+					console.timeStart = clock();
+				}
 				break;
 			case eKeyCode::KEY_RIGHT:
-				player.AddPosition(1, 0);
+				if (IsMoveAvailable(1, 0))
+				{
+					g_player.AddPosition(1, 0);
+					console.timeStart = clock();
+				}
 				break;
 			case eKeyCode::KEY_SPACE:
-				player.SetNextDirection();
+				if (IsRotateAvailable())
+				{
+					g_player.SetNextDirection();
+					console.timeStart = clock();
+				}
 				break;
 		}
+	}
+}
+
+/**
+@brief		Function that auto down move
+@param
+@return
+*/
+void AutoDown()
+{
+	double dTimeDiff = clock() - console.timeStart;
+
+	if (dTimeDiff >= 1000)
+	{
+		console.timeStart = clock();
+
+		g_player.AddPosition(0, 1);
 	}
 }
 
@@ -330,12 +511,14 @@ int main(void)
 	while (true)
 	{
 		InputKey();
-		DrawPlayer();
-		DrawFrame();
+		CalcPlayer();
+		Render();
+		CheckBottom();
+		AutoDown();
 
-		Sleep(10);
 		ClearScreen();
 		BufferFlip();
+		Sleep(10);
 	}
 
 	DestroyGame();
